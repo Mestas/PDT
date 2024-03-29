1e3dc5e40c2a6f9a40805e6f4ab1c49da31bc04d
Mestas <52190560+Mestas@users.noreply.github.com>
2024-03-29 14:02:21 +0800

    Update 08_🌈_TFMaster薄膜光学仿真工具 (V1.1).py

--- pages/08_🌈_TFMaster薄膜光学仿真工具 (V1.1).py
+++ pages/08_🌈_TFMaster薄膜光学仿真工具 (V1.1).py
@@ -796,7 +796,7 @@ st.markdown(
         height: 70px !important;
         width: 150px !important;
     }
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi2 > div > div > div > div:nth-child(13) > div.st-emotion-cache-12d0wtz.e1f1d6gn3 > div > div > div > div:nth-child(2) > div > button
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div:nth-child(13) > div.st-emotion-cache-12d0wtz.e1f1d6gn3 > div > div > div > div:nth-child(1) > div
     {
     background-color: rgb(220, 240, 220);
     height: 70px !important;
