478326bb5a8d8fa16be46561e1f57a48d6355612
Mestas <52190560+Mestas@users.noreply.github.com>
2024-04-09 10:30:30 +0800

    Update 03_🌝_亮度波动概率分布预估工具 (V1.0).py

--- pages/03_🌝_亮度波动概率分布预估工具 (V1.0).py
+++ pages/03_🌝_亮度波动概率分布预估工具 (V1.0).py
@@ -1155,7 +1155,7 @@ except NameError:
 st.markdown(
     '''
     <style>
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(8) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div > div > button
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-gh2jqd.ea3mdgi5 > div > div > div > div:nth-child(8) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div > div > button
     {
     background-color: rgb(220, 240, 220) !important;
     height: 70px !important;
@@ -1170,7 +1170,7 @@ st.markdown(
 st.markdown(
     '''
     <style>
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(4) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div > div > section > button
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-gh2jqd.ea3mdgi5 > div > div > div > div:nth-child(4) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div > div > section > button
     {
     background-color: rgb(220, 240, 220) !important;
     }
