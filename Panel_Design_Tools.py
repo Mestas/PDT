77fbc28ee11b153af7615772bff61c9b24fa4e98
Mestas <52190560+Mestas@users.noreply.github.com>
2024-04-09 10:46:32 +0800

    Update Panel_Design_Tools.py

--- Panel_Design_Tools.py
+++ Panel_Design_Tools.py
@@ -110,10 +110,11 @@ if btn is True:
 st.markdown(
     '''
     <style>
-    baseButton-secondary {
-        background-color: rgb(220, 240, 220) !important;
-        height: 70px !important;
-        width: 150px !important;
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-gh2jqd.ea3mdgi5 > div > div > div > div:nth-child(6) > div.st-emotion-cache-ndxjbj.e1f1d6gn3 > div > div > div > div:nth-child(2) > div > button
+    {
+    background-color: rgb(220, 240, 220) !important;
+    height: 70px !important;
+    width: 150px !important;
     }
     input {
         background-color: rgb(220, 240, 220) !important;
