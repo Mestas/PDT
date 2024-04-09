b5c9af4712fb8f8f105e1824f3aee447fdfe538e
Mestas <52190560+Mestas@users.noreply.github.com>
2024-04-09 10:25:29 +0800

    Update 03_🌝_亮度波动概率分布预估工具 (V1.0).py

--- pages/03_🌝_亮度波动概率分布预估工具 (V1.0).py
+++ pages/03_🌝_亮度波动概率分布预估工具 (V1.0).py
@@ -1155,7 +1155,7 @@ except NameError:
 st.markdown(
     '''
     <style>
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(8) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div > div > button
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(8) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div > div > button
     {
     background-color: rgb(220, 240, 220);
     height: 70px !important;
@@ -1170,7 +1170,7 @@ st.markdown(
 st.markdown(
     '''
     <style>
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(4) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div > div > section > button
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(4) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div > div > section > button
     {
     background-color: rgb(220, 240, 220);
     }
@@ -1183,13 +1183,10 @@ st.markdown(
 st.markdown(
     '''
     <style>
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(6) > div.st-emotion-cache-1z094xl.e1f1d6gn3 > div > div > div > div:nth-child(1) > div > div > div > div
-    {
-    background-color: rgb(220, 240, 220);
-    }
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(6) > div.st-emotion-cache-1z094xl.e1f1d6gn3 > div > div > div > div:nth-child(2) > div > div > div > div
-    {
-    background-color: rgb(220, 240, 220);
+    input {
+        background-color: rgb(220, 240, 220) !important; /*设置背景颜色*/
+        text-align: center !important; /*设置字体水平居中*/
+        vertical-align: middle !important; /*设置字体垂直居中*/
     }
     </style>
     ''',
