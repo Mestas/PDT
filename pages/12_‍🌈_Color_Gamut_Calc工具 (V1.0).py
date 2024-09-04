c29eb206dcb1dbda7f3affc13745c96b4d272752
Mestas <52190560+Mestas@users.noreply.github.com>
2024-09-04 16:21:16 +0800

    Update 12_‍🌈_Color_Gamut_Calc工具 (V1.0).py

--- pages/12_‍🌈_Color_Gamut_Calc工具 (V1.0).py
+++ pages/12_‍🌈_Color_Gamut_Calc工具 (V1.0).py
@@ -221,29 +221,29 @@ st.markdown(
     {
     background-color: rgb(220, 240, 220);
     }
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(5) > div.st-emotion-cache-14nh3c1.e1f1d6gn3 > div > div > div > div > div > div > div
-    {
-    background-color: rgb(220, 240, 220);
-    }
+
     
-    
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(6) > div:nth-child(3) > div > div > div > div:nth-child(1) > div > div > div > div
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(6) > div:nth-child(2) > div > div > div > div:nth-child(1) > div > div > div > div
     {
     background-color: rgb(220, 240, 220);
     }
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(6) > div:nth-child(2) > div > div > div > div:nth-child(2) > div > div > div > div
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(6) > div:nth-child(3) > div > div > div > div:nth-child(1) > div > div > div > div
     {
     background-color: rgb(220, 240, 220);
     }
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(6) > div:nth-child(3) > div > div > div > div:nth-child(2) > div > div > div > div
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(6) > div:nth-child(2) > div > div > div > div:nth-child(2) > div > div > div > div
     {
     background-color: rgb(220, 240, 220);
     }
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(6) > div:nth-child(2) > div > div > div > div:nth-child(3) > div > div > div > div
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(6) > div:nth-child(3) > div > div > div > div:nth-child(2) > div > div > div > div
     {
     background-color: rgb(220, 240, 220);
     }
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(6) > div:nth-child(3) > div > div > div > div:nth-child(3) > div > div > div > div
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(6) > div:nth-child(2) > div > div > div > div:nth-child(3) > div > div > div > div
+    {
+    background-color: rgb(220, 240, 220);
+    }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(6) > div:nth-child(3) > div > div > div > div:nth-child(3) > div > div > div > div
     {
     background-color: rgb(220, 240, 220);
     }
