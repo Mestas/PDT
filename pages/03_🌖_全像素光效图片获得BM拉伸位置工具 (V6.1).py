b395132c87d6e77697b5b5763e183dbaf09cc832
Mestas <52190560+Mestas@users.noreply.github.com>
2024-09-04 15:43:11 +0800

    Update 03_🌖_全像素光效图片获得BM拉伸位置工具 (V6.1).py

--- pages/03_🌖_全像素光效图片获得BM拉伸位置工具 (V6.1).py
+++ pages/03_🌖_全像素光效图片获得BM拉伸位置工具 (V6.1).py
@@ -322,7 +322,7 @@ except NameError:
 st.markdown(
     '''
     <style>
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi5 > div > div > div > div:nth-child(10) > div.st-emotion-cache-17xod8c.e1f1d6gn3 > div > div > div > div > div > button
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(10) > div.st-emotion-cache-17xod8c.e1f1d6gn3 > div > div > div > div > div > button
     {
     background-color: rgb(220, 240, 220);
     height: 70px !important;
