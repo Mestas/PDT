bccf1a8880c7c140aef5b5064cf0bdde66e204b3
Mestas <52190560+Mestas@users.noreply.github.com>
2024-09-04 16:49:19 +0800

    Update 03_🌖_全像素光效图片获得BM拉伸位置工具 (V6.1).py

--- pages/03_🌖_全像素光效图片获得BM拉伸位置工具 (V6.1).py
+++ pages/03_🌖_全像素光效图片获得BM拉伸位置工具 (V6.1).py
@@ -341,6 +341,10 @@ st.markdown(
     {
     background-color: rgb(220, 240, 220);
     }
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(4) > div.st-emotion-cache-ulq27u.e1f1d6gn3 > div > div > div > div > div > section > button
+    {
+    background-color: rgb(220, 240, 220);
+    }
     </style>
     ''',
     unsafe_allow_html=True
