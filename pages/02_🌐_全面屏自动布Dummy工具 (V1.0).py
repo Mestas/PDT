0c0d8d6942cef494ae1c5c5be49993d314a4242b
Mestas <52190560+Mestas@users.noreply.github.com>
2024-09-04 15:42:16 +0800

    Update 02_🌐_全面屏自动布Dummy工具 (V1.0).py

--- pages/02_🌐_全面屏自动布Dummy工具 (V1.0).py
+++ pages/02_🌐_全面屏自动布Dummy工具 (V1.0).py
@@ -795,7 +795,7 @@ if Generate is True:
 st.markdown(
     '''
     <style>
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-bm2z3a.ea3mdgi8 > div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div:nth-child(12) > div.st-emotion-cache-vft1hk.e1f1d6gn3 > div > div > div > div > div > button
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(12) > div.st-emotion-cache-vft1hk.e1f1d6gn3 > div > div > div > div > div > button
     {
     background-color: rgb(220, 240, 220);
     height: 70px !important;
