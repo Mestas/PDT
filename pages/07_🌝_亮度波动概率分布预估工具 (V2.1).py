33d422cf11517bbd69d828bdc0817bd7986a2bb3
Mestas <52190560+Mestas@users.noreply.github.com>
2024-09-04 15:45:31 +0800

    Update 07_🌝_亮度波动概率分布预估工具 (V2.1).py

--- pages/07_🌝_亮度波动概率分布预估工具 (V2.1).py
+++ pages/07_🌝_亮度波动概率分布预估工具 (V2.1).py
@@ -1302,7 +1302,7 @@ except FileNotFoundError:
 st.markdown(
     '''
     <style>
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-bm2z3a.ea3mdgi8 > div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div:nth-child(8) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div.element-container.st-emotion-cache-1sx39vv.e1f1d6gn4 > div > button
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(8) > div.st-emotion-cache-15qpf3q.e1f1d6gn3 > div > div > div > div > div > button
     {
     background-color: rgb(220, 240, 220) !important;
     height: 70px !important;
