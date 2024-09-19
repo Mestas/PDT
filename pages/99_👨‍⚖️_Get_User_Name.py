f0479a759bcb2ce162637b6b44005420f700f663
Mestas <52190560+Mestas@users.noreply.github.com>
2024-09-19 16:04:11 +0800

    Update 99_👨‍⚖️_Get_User_Name.py

--- pages/99_👨‍⚖️_Get_User_Name.py
+++ pages/99_👨‍⚖️_Get_User_Name.py
@@ -165,7 +165,7 @@ st.markdown(
         height: 60px;
         width: 120px;
     }
-    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(3) > div.st-emotion-cache-j5r0tf.e1f1d6gn3 > div > div > div > div > div > div
+    #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi3 > div.block-container.st-emotion-cache-1y4p8pa.ea3mdgi2 > div > div > div > div:nth-child(3) > div.st-emotion-cache-12w0qpk.e1f1d6gn3 > div > div > div > div > div > div > div
     {
         background-color: rgb(220, 240, 220);
     }
